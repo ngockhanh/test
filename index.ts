b0b5a0a43319f9630f20dcb673ff825c4e3a8d50
khanh.tran <khanh.tran@savvycomsoftware.com>
2023-04-20 14:59:53 +0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+export const getMaxProfit = (stockPriceList: number[]): number {
+    let { profit, buyPrice } = stockPriceList.reduce(
+    
+      ({ profit, buyPrice }, price) => ({
+        profit: Math.max(profit, price - buyPrice),
+        buyPrice: Math.min(buyPrice, price),
+      }),
+      { profit: 0, buyPrice: stockPriceList[0] }
+    );
+    
+  
+    return profit;
+  }
